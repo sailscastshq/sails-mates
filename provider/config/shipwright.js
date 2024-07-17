72bc87bb175ecc8f2d13513bcb7c7e5bf7750a14
Kelvin Oghenerhoro Omereshone <kelvinomereshone@gmail.com>
2024-07-17 12:48:54 +0100

    chore: setup provider

--- provider/config/shipwright.js
+++ provider/config/shipwright.js
@@ -1,6 +1,22 @@
 const { pluginReact } = require('@rsbuild/plugin-react')
+const { ModuleFederationPlugin } = require('@module-federation/enhanced/rspack')
+
 module.exports.shipwright = {
   build: {
-    plugins: [pluginReact()]
-  }
+    tools: {
+      rspack: (config, { appendPlugins }) => {
+        config.output.uniqueName = 'federation_provider'
+        appendPlugins([
+          new ModuleFederationPlugin({
+            name: 'federation_provider',
+            exposes: {
+              './Counter': './assets/js/components/Counter.jsx',
+            },
+            shared: ['react', 'react-dom'],
+          }),
+        ])
+      },
+    },
+    plugins: [pluginReact()],
+  },
 }
