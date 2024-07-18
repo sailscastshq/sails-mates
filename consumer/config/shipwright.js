f10c93aeb0f4498e96ee7f37015439d20cf1d297
Kelvin Oghenerhoro Omereshone <kelvinomereshone@gmail.com>
2024-07-18 09:07:47 +0100

    fix: expand shared config

--- consumer/config/shipwright.js
+++ consumer/config/shipwright.js
@@ -1,10 +1,15 @@
 const { pluginReact } = require('@rsbuild/plugin-react')
 const { ModuleFederationPlugin } = require('@module-federation/enhanced/rspack')
-
+const { dependencies } = require('../package.json')
 module.exports.shipwright = {
   build: {
+    dev: {
+      // It is necessary to configure assetPrefix, and in the production build, you need to configure output.assetPrefix
+      assetPrefix: 'http://localhost:1337',
+    },
     tools: {
       rspack: (config, { appendPlugins }) => {
+        config.publicPath = 'auto'
         appendPlugins([
           new ModuleFederationPlugin({
             name: 'federation_consumer',
@@ -12,7 +17,17 @@ module.exports.shipwright = {
               federation_provider:
                 'federation_provider@http://localhost:1338/mf-manifest.json',
             },
-            shared: ['react', 'react-dom'],
+            shared: {
+              ...dependencies,
+              react: {
+                singleton: true,
+                requiredVersion: dependencies.react,
+              },
+              'react-dom': {
+                singleton: true,
+                requiredVersion: dependencies['react-dom'],
+              },
+            },
           }),
         ])
       },
--- provider/config/shipwright.js
+++ provider/config/shipwright.js
@@ -1,16 +1,33 @@
 const { pluginReact } = require('@rsbuild/plugin-react')
 const { ModuleFederationPlugin } = require('@module-federation/enhanced/rspack')
+const { dependencies } = require('../package.json')
 
 module.exports.shipwright = {
   build: {
+    dev: {
+      // It is necessary to configure assetPrefix, and in the production build, you need to configure output.assetPrefix
+      assetPrefix: 'http://localhost:1338',
+    },
     tools: {
       rspack: (config, { appendPlugins }) => {
         config.output.uniqueName = 'federation_provider'
+        config.publicPath = 'auto'
         appendPlugins([
           new ModuleFederationPlugin({
             name: 'federation_provider',
             exposes: {
               './counter': './assets/js/components/Counter.jsx',
+            },
+            shared: {
+              ...dependencies,
+              react: {
+                singleton: true,
+                requiredVersion: dependencies.react,
+              },
+              'react-dom': {
+                singleton: true,
+                requiredVersion: dependencies['react-dom'],
+              },
             },
           }),
         ])
