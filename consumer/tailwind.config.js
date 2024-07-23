f0145c11b6c2137a573ba6da62cfd915c059f015
Kelvin Oghenerhoro Omereshone <kelvinomereshone@gmail.com>
2024-07-23 22:01:56 +0100

    fix: resolve missing tailwind styles

--- consumer/tailwind.config.js
+++ consumer/tailwind.config.js
@@ -1,7 +1,17 @@
 module.exports = {
   content: ['./views/**/*.ejs', './assets/js/**/*.{js,jsx,ts,tsx}'],
+  safelist: [
+    'rounded-lg',
+    'border',
+    'border-purple-500',
+    'px-4',
+    'py-2',
+    'text-sm',
+    'text-gray-600',
+    'hover:border-purple-600',
+  ],
   theme: {
-    extend: {}
+    extend: {},
   },
-  plugins: []
+  plugins: [],
 }
