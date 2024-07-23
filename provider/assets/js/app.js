bdcdd55cd214659417ccc668b9f7bafa09f41937
Kelvin Oghenerhoro Omereshone <kelvinomereshone@gmail.com>
2024-07-23 17:31:45 +0100

    fix: async import

--- provider/assets/js/app.js
+++ provider/assets/js/app.js
@@ -1,10 +1,1 @@
-import { createInertiaApp } from '@inertiajs/react'
-import { createRoot } from 'react-dom/client'
-import '~/css/main.css'
-
-createInertiaApp({
-  resolve: (name) => require(`./pages/${name}`),
-  setup({ el, App, props }) {
-    createRoot(el).render(<App {...props} />)
-  }
-})
+import('./bootstrap')
