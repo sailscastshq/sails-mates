9f95b30431cedea924c38d6fa29bd97e59e29928
Kelvin Oghenerhoro Omereshone <kelvinomereshone@gmail.com>
2024-08-28 00:43:15 +0100

    chore: PoC federated Sails actions

--- consumer/config/routes.js
+++ consumer/config/routes.js
@@ -8,16 +8,11 @@
  * http://sailsjs.com/anatomy/config/routes-js
  */
 
-module.exports.routes = {
-  /***************************************************************************
-   *                                                                          *
-   * Custom routes here...                                                    *
-   *                                                                          *
-   * If a request to a URL doesn't match any of the custom routes above, it   *
-   * is matched against Sails route blueprints. See `config/blueprints.js`    *
-   * for configuration options and examples.                                  *
-   *                                                                          *
-   ***************************************************************************/
-  'GET /': 'home/index',
-  'GET /example': 'example/index',
+module.exports.routes = async () => {
+  const { actions } = await import('federated_actions/actions')
+
+  return {
+    'GET /example': actions['example/index'],
+    // ... other routes
+  }
 }
