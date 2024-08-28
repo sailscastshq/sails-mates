b5d002717dbc0ad740cb068dad49c7f409d87e7b
Kelvin Oghenerhoro Omereshone <kelvinomereshone@gmail.com>
2024-08-28 21:05:43 +0100

    chore: move plugins to shared config

--- provider/config/shipwright.js
+++ provider/config/shipwright.js
@@ -1,63 +1,63 @@
 const { pluginReact } = require('@rsbuild/plugin-react')
 const { ModuleFederationPlugin } = require('@module-federation/enhanced/rspack')
 const { NodeFederationPlugin } = require('@module-federation/node')
-const { dependencies } = require('../package.json')
 
 module.exports.shipwright = {
   build: {
-    source: {
-      entry: {
-        federatedActions: './assets/js/federated-actions.js',
+    environments: {
+      web: {
+        output: {
+          target: 'web',
+        },
+        tools: {
+          rspack: (config, { appendPlugins }) => {
+            appendPlugins([
+              new ModuleFederationPlugin({
+                name: 'federation_provider',
+                exposes: {
+                  './counter': './assets/js/components/Counter.jsx',
+                },
+                shared: {
+                  react: {
+                    singleton: true,
+                    requiredVersion: dependencies['react'],
+                  },
+                  'react-dom': {
+                    singleton: true,
+                    requiredVersion: dependencies['react-dom'],
+                  },
+                },
+              }),
+            ])
+          },
+        },
+      },
+      node: {
+        source: {
+          entry: {
+            federatedActions: './assets/js/federated-actions.js',
+          },
+        },
+        output: {
+          target: 'node',
+        },
+        tools: {
+          rspack: (config, { appendPlugins }) => {
+            config.externals = ['fs', 'path']
+            appendPlugins([
+              new NodeFederationPlugin({
+                name: 'federation_provider',
+                library: { type: 'commonjs-module' },
+                exposes: {
+                  './actions': './assets/js/federated-actions.js',
+                },
+                shared: ['sails'],
+              }),
+            ])
+          },
+        },
       },
     },
-
-    dev: {
-      // It is necessary to configure assetPrefix, and in the production build, you need to configure output.assetPrefix
-      assetPrefix: 'http://localhost:1338',
-    },
-    tools: {
-      rspack: (config, { appendPlugins }) => {
-        if (config.entry.federatedActions) {
-          config.target = 'node'
-          config.externals = ['fs', 'path']
-        }
-        config.output.uniqueName = 'federation_provider'
-        config.publicPath = 'auto'
-        appendPlugins([
-          new ModuleFederationPlugin({
-            name: 'federation_provider',
-            exposes: {
-              './counter': './assets/js/components/Counter.jsx',
-            },
-            shared: {
-              ...dependencies,
-              react: {
-                singleton: true,
-                requiredVersion: dependencies.react,
-              },
-              'react-dom': {
-                singleton: true,
-                requiredVersion: dependencies['react-dom'],
-              },
-            },
-          }),
-        ])
-        if (config.target === 'node') {
-          appendPlugins([
-            new NodeFederationPlugin({
-              name: 'federated_provider',
-              library: {
-                type: 'commonjs-module',
-              },
-              exposes: {
-                './actions': './assets/js/federated-actions.js',
-              },
-              shared: ['sails'],
-            }),
-          ])
-        }
-      },
-    },
-    plugins: [pluginReact()],
   },
+  plugins: [pluginReact()],
 }
--- provider/federated-actions.js
+++ provider/federated-actions.js
@@ -12,13 +12,13 @@ function readControllers(dir) {
     } else if (file.endsWith('.js')) {
       const relativePath = path.relative(dir, filePath)
       const routePath = relativePath.replace(/\\/g, '/').replace('.js', '')
-      const action = require(`../../api/controllers/${routePath}`)
+      const action = require(`./api/controllers/${routePath}`)
       action._federated = true
       controllers[routePath] = action
     }
   })
 }
 
-readControllers(path.join(__dirname, '../api/controllers'))
+readControllers(path.join(__dirname, './api/controllers'))
 
 module.exports = controllers
